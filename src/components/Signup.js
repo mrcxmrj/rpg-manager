78ca4785180180fdfff9a4fa7e40e812e8825db0
mrcxmrj <marcin.zurawel@gmail.com>
2021-08-02 16:38:58 +0200

    added button disable when loading signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "./useForm";
 import { useAuth } from "../contexts/authContext";
+import { Link } from "react-router-dom";
 
 export const Signup = () => {
     const validate = (values) => {
@@ -26,7 +27,9 @@ export const Signup = () => {
         Email: ${values.email}
         Password: ${values.password}`);
     }; */
-    const { signup, currentUser } = useAuth();
+    const { signup } = useAuth();
+    const [isLoading, setIsLoading] = useState(false);
+
     // takes key:value object (values) and uses signup on email and password
     const handleSignup = (values) => {
         //processes error code from firebase (for example email already in use)
@@ -44,9 +47,10 @@ export const Signup = () => {
             }
         };
 
-        signup(values.email, values.password).catch((error) =>
-            processErrorCode(error)
-        );
+        setIsLoading(true);
+        signup(values.email, values.password)
+            .catch((error) => processErrorCode(error))
+            .finally(() => setIsLoading(false));
     };
 
     const { values, errors, setErrors, handleChange, handleSubmit } = useForm(
@@ -55,52 +59,59 @@ export const Signup = () => {
     );
 
     return (
-        <form action="submit" onSubmit={handleSubmit} noValidate>
-            <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    onChange={handleChange}
-                    value={values.email || ""}
-                    required
-                />
-                {errors.email && (
-                    <div className="form-error">{errors.email}</div>
-                )}
+        <div>
+            <form action="submit" onSubmit={handleSubmit} noValidate>
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        onChange={handleChange}
+                        value={values.email || ""}
+                        required
+                    />
+                    {errors.email && (
+                        <div className="form-error">{errors.email}</div>
+                    )}
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        onChange={handleChange}
+                        value={values.password || ""}
+                        required
+                    />
+                    {errors.password && (
+                        <div className="form-error">{errors.password}</div>
+                    )}
+                </div>
+                <div className="form-group">
+                    <label htmlFor="confirmPassword">Confirm password</label>
+                    <input
+                        type="password"
+                        name="confirmPassword"
+                        id="confirmPassword"
+                        onChange={handleChange}
+                        value={values.confirmPassword || ""}
+                        required
+                    />
+                    {errors.confirmPassword && (
+                        <div className="form-error">
+                            {errors.confirmPassword}
+                        </div>
+                    )}
+                </div>
+                <button type="submit" disabled={isLoading}>
+                    Sign up
+                </button>
+            </form>
+            <div>
+                Already have an account? <Link to="/login">Log In</Link>
             </div>
-            <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    onChange={handleChange}
-                    value={values.password || ""}
-                    required
-                />
-                {errors.password && (
-                    <div className="form-error">{errors.password}</div>
-                )}
-            </div>
-            <div className="form-group">
-                <label htmlFor="confirmPassword">Confirm password</label>
-                <input
-                    type="password"
-                    name="confirmPassword"
-                    id="confirmPassword"
-                    onChange={handleChange}
-                    value={values.confirmPassword || ""}
-                    required
-                />
-                {errors.confirmPassword && (
-                    <div className="form-error">{errors.confirmPassword}</div>
-                )}
-            </div>
-
-            <button type="submit">Sign up</button>
-            {JSON.stringify(currentUser)}
-        </form>
+        </div>
     );
 };
