6e9c70de3bc1095f6786e83291a27d7b601b3582
mrcxmrj <marcin.zurawel@gmail.com>
2021-08-09 00:10:58 +0200

    added basic campaign info display

--- src/components/Campaign.js
+++ src/components/Campaign.js
@@ -1,8 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { db } from "../firebase";
 
 export const Campaign = () => {
     const { id } = useParams();
+    const [data, setData] = useState({
+        players: [{ username: "player1", uid: "asdf" }, "player2"],
+    });
+    const [loading, setLoading] = useState(true);
 
-    return <>{id}</>;
+    useEffect(() => {
+        const unsubscribe = db
+            .collection("campaigns")
+            .doc(id)
+            .onSnapshot((doc) => {
+                console.log("fetching campaign data...");
+                setData((currentData) => ({ ...currentData, ...doc.data() }));
+                setLoading(false);
+            });
+
+        return unsubscribe;
+    }, [id]);
+
+    console.log(data);
+
+    // requests, pending requests, although maybe that should be in the gm panel section
+
+    return (
+        !loading && (
+            <div>
+                <h1>{data.name}</h1>
+                <ul>
+                    <li>gm: {data.gm}</li>
+                    <li>gmUid: {data.gmUid}</li>
+                    <li>description: {data.description || ""}</li>
+                    <li>system: {data.system || ""}</li>
+                    <li>
+                        players:
+                        <ol>
+                            {data.players.map((player) => (
+                                <li>{player}</li>
+                            ))}
+                        </ol>
+                    </li>
+                </ul>
+            </div>
+        )
+    );
 };
